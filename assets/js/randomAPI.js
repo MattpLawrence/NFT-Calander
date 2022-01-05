60d0d70dfe421ffe3f95627ec917a18c94f48bbd
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-05 15:31:27 -0500

    finished random button
    matt and rebecca finished the functionality of the random button

--- assets/js/randomAPI.js
+++ assets/js/randomAPI.js
@@ -5,27 +5,38 @@
 // 		"x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436"
 // 	}
 // })
-fetch("https://kareoke.p.rapidapi.com/v1/song/random", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "kareoke.p.rapidapi.com",
-		"x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436"
-	}
-})
-.then(function (response) {
-  if (!response.ok) {
-    throw response.json();
-  }
-  console.log(response)
-  return response.json();
-})
-.then(function (data){
-  console.log(data);
-  var randomID = data.youtube.id;
-  console.log(data.youtube.id);
 
+function randomVid (){
+  fetch("https://kareoke.p.rapidapi.com/v1/song/random", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "kareoke.p.rapidapi.com",
+      "x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436"
+    }
+  })
+  .then(function (response) {
+    if (!response.ok) {
+      throw response.json();
+    }
+    console.log(response)
+    return response.json();
+  })
+  .then(function (data){
+    console.log(data);
+    var searchVal = data.youtube.id;
+    console.log(data.youtube.id);
+    searchVid(searchVal);
+
+  })
+  .catch(err => {
+    console.error(err);
+  }); 
+}
+var randomBtnEl = $('#randomBtn');
+
+$(randomBtnEl).on('click', function(){
+  console.log('hewwo?')
   
+  randomVid();
+
 })
-.catch(err => {
-	console.error(err);
-}); 

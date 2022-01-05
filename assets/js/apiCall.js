f492870f7ba488bb732c9c3c4179f67b6a845cf0
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-04 21:35:35 -0500

    tried another API, also worked, still no image.

--- assets/js/apiCall.js
+++ assets/js/apiCall.js
@@ -1,7 +1,18 @@
-fetch("https://top-nft-sales.p.rapidapi.com/sales/30d", {
+// https://rapidapi.com/novus-api-novus-api-default/api/top-nft-sales/
+// fetch("https://top-nft-sales.p.rapidapi.com/sales/30d", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "top-nft-sales.p.rapidapi.com",
+// 		"x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436"
+// 	}
+// })
+
+
+https://rapidapi.com/tomerdad/api/nft-stats/
+fetch("https://nft-stats.p.rapidapi.com/NFT/topNFTs/?Filter=%3CREQUIRED%3E", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "top-nft-sales.p.rapidapi.com",
+		"x-rapidapi-host": "nft-stats.p.rapidapi.com",
 		"x-rapidapi-key": "4358a3ae45msh1ef514db96f084bp1426f0jsn143dce70b436"
 	}
 })

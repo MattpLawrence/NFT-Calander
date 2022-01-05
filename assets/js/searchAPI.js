5c03e14cbb29f988e88b8f633d2e550dd72bf3c7
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-05 13:58:22 -0500

    search funtion working

--- assets/js/searchAPI.js
+++ assets/js/searchAPI.js
@@ -20,6 +20,44 @@ function searchVid(searchVal){
     console.log(data.items[0].snippet);
     var id = data.items[0].id.videoId;
     console.log(id);
+    searchResultsLoop(data)
   });
 
+}
+
+function searchResultsLoop(data) {
+  $('main').empty();
+
+  $.each(data.items, function (i, item) {
+
+      var thumb = item.snippet.thumbnails.medium.url;
+      var title = item.snippet.title;
+      var desc = item.snippet.description.substring(0, 100);
+      var vid = item.snippet.resourceId;
+
+      
+      $('main').append(`
+        <article class="item" data-key="${vid}">
+
+          <img src="${thumb}" alt="" class="thumb">
+          <div class="details">
+            <h4>${title}</h4>
+            <p>${desc}</p>
+          </div>
+
+        </article>
+      `);
+  });
+  var id = data.items[0].snippet.thumbnails.high.url;
+  mainImgSearch(id);
+  
+  function mainImgSearch(id) {
+    $('#preview').html($('<img>',{id: 'image', src: id, width:'560', height: '315'}  
+    ));
+  }
+}
+
+function mainImgSearch(id) {
+  $('#preview').html($('<img>',{id: 'image', src: id, width:'560', height: '315'}  
+  ));
 }

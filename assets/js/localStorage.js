013bbfbcf9d9b6b019148f81f83f9207956a0e27
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-06 22:51:30 -0500

    Tried several functions for the search history to save in local storage.

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -5,14 +5,37 @@ function searchHistory(id) {
 }
 
 
-let searchHistory = localStorage.getItem ("searchVal");
-console.log (searchHistory);
+// let searchHistory = localStorage.getItem ("search");
+// // console.log (searchHistory);
 
-var searchHistory
-function searchHistory () {
-  var prevVideo = localStorage.getItem('player');
+// var searchHistory
 
-  document.getElementById('player');
 
-}
-console.log(searchHistory)
+
+
+
+
+// function searchResultsLoop(data) {
+//     $('main').empty();
+  
+//     $.each(data.items, function (i, item) {
+  
+//         var thumb = item.snippet.thumbnails.medium.url;
+//         var title = item.snippet.title;
+//         var desc = item.snippet.description.substring(0, 100);
+//         var vid = item.snippet.resourceId;
+  
+        
+//         $('main').append(`
+//           <article class="item" data-key="${vid}">
+  
+//             <img src="${thumb}" alt="" class="thumb">
+//             <div class="details">
+//               <h4>${title}</h4>
+//               <p>${desc}</p>
+//             </div>
+  
+//           </article>
+//         `);
+
+

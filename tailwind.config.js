1475e23cdbc48f9dfea1080b46f2fdd4ec3f3ee2
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-08 10:20:15 -0500

    background added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,13 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: theme => ({
-                "banner-image": "url()"
+                "banner-image": "url(assets/23BE4F19-B513-47D7-8F59-A1E243918395.jpeg)"
             })
         }
+       inset: {
+           "50": "50px",
+           "100": "100px",
+           "150": "150px"
+       } 
     }
 }

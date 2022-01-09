144331a920a75dd927e14bc0c5131c2d6a1ffc95
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-09 15:19:13 -0500

    linked apiCall random btn to landingPage Html, console works

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,15 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  plugins: [
+    plugin(function({ addComponents }) {
+      addComponents({
+        '.btn': {
+          padding: '.5rem 1rem',
+          borderRadius: '.25rem',
+          fontWeight: '600',
+        },
+      })
+    })
+  ]
+}
